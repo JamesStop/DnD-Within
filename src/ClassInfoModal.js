3c089b65ae220055b8898db5e7293c35cd5e3d8e
xelphite <stoppanij@gmail.com>
2022-06-19 17:19:40 -0700

    further tuned the modal styling

--- src/ClassInfoModal.js
+++ src/ClassInfoModal.js
@@ -3,29 +3,31 @@ import { Link } from 'react-router-dom';
 
 function ClassInfoModal({ currentChar, setCurrentChar }) {
 	function cancelClass() {
-        setCurrentChar({...currentChar, class: null})
-    }
+		setCurrentChar({ ...currentChar, class: null });
+	}
 
 	if (currentChar.class) {
 		return (
 			<div className='info-modal'>
 				<section className='modal-info'>
 					<div className='modal-head-wrapper'>
-						<button onClick={cancelClass}>close</button>
-						{currentChar.class}
+						<h3 className='modal-title'>{currentChar.class}</h3>
+						<button onClick={cancelClass} className='close-modal-button'>
+							X
+						</button>
 					</div>
 					<div className='modal-body-wrapper'></div>
 					<div className='modal-foot-wrapper'>
 						<Link to='/Races'>
-							<button>choose</button>
+							<button className='choose-button'>choose</button>
 						</Link>
 					</div>
 				</section>
 			</div>
 		);
 	} else {
-        return null;
-    }
+		return null;
+	}
 }
 
 export default ClassInfoModal;
