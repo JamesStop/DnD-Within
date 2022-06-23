19547fceae9b38b3883606abaa099221b70f7e98
xelphite <stoppanij@gmail.com>
2022-06-23 12:33:00 -0700

    started styling current character display

--- src/CurrentCharDisplay.js
+++ src/CurrentCharDisplay.js
@@ -5,10 +5,19 @@ function CurrentCharDisplay({currentChar}) {
 
 
     return (
-        <div>
-            {currentChar.charName}
-        </div>
-    );
+			<div className='character-info-display'>
+				<p className='character-name-display'>{currentChar.charName}</p>
+				<p className='character-level-display'>Level: {currentChar.currentLevel}</p>
+                <div className="more-info-display">
+                    <section>
+                        <p>Class: {currentChar.class}</p>
+                    </section>
+                    <section>
+                        <p>Race: {currentChar.race}</p>
+                    </section>
+                </div>
+			</div>
+		);
 }
 
 export default CurrentCharDisplay;
